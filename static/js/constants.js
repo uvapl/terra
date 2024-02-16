b99be6a2b12e9619ccaf2ac329ce6c3afb6f2d8b
Kim 金可明 <koomen@protonmail.com>
2024-02-16 19:16:58 +0100

    docs: add space to fix misalignd asterisk in docblock

--- static/js/constants.js
+++ static/js/constants.js
@@ -17,6 +17,6 @@ const AUTOSAVE_INTERVAL = 60 * 1000;
  * To prevent each user will do a POST request for the auto-save at the exact
  * same time, each user will start the app with a time offset between 0 and the
  * AUTOSAVE_START_OFFSET value. After this time, the actual timer will start.
-* The offset is defined in milliseconds.
+ * The offset is defined in milliseconds.
  */
 const AUTOSAVE_START_OFFSET = 60 * 1000;
