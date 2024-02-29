4fab516f63a6d97e981797bb3cd92643e5323943
Kim 金可明 <koomen@protonmail.com>
2024-02-29 18:30:28 +0100

    feat: implement `BaseAPI` that other APIs can extend

---
+++ static/js/workers/base-api.js
@@ -0,0 +1,40 @@
+class NotImplemented extends Error {
+  constructor(modname, fieldname) {
+    super(`${modname}.${fieldname} not implemented.`);
+  }
+}
+
+/**
+ * Base class for API implementations for each programming language worker.
+ */
+class BaseAPI {
+  constructor(options) {
+    for (const fn of ['hostWrite', 'compileLinkRunCallback']) {
+      if (!(options[fn] instanceof Function)) {
+        throw new Error(`Missing required option: ${fn}`);
+      }
+    }
+
+    this.hostWrite = options.hostWrite;
+    this.compileLinkRunCallback = options.compileLinkRunCallback;
+  }
+
+  /**
+   * Write back to the terminal.
+   *
+   * @param {string} message - The message to print.
+   */
+  hostWriteCmd(message) {
+    this.hostWrite(`\$ ${message}\n`);
+  }
+
+  /**
+   * Abstract method to compile, link, and run the user's code.
+   *
+   * @async
+   * @throws {NotImplemented} - This method must be implemented by the subclass.
+   */
+  async compileLinkRun() {
+    throw NotImplemented('BaseAPI', 'compileLinkRun');
+  }
+}
