4464b423f67ced587e497fc1f6de6c69ac70d8d2
Kim 金可明 <koomen@protonmail.com>
2024-03-21 15:26:23 +0100

    fix: clear globals and locals after running the user code

--- static/js/workers/py.worker.js
+++ static/js/workers/py.worker.js
@@ -128,9 +128,12 @@ class API extends BaseAPI {
     // Clear the standard output.
     this.pyodide.runPython('sys.stdout = io.StringIO()')
 
+    // Gather the current globals (i.e. vars, funcs, classes).
+    const globals = this.pyodide.globals.get('dict')();
+
     try {
       // Run the code and get the standard output.
-      let cmdOutput = this.pyodide.runPython(code.join('\n'))
+      let cmdOutput = this.pyodide.runPython(code.join('\n'), { globals, locals: globals });
       const stdout = this.pyodide.runPython('sys.stdout.getvalue()')
 
       // In most cases, the commands will write to the stdout, but some packages,
@@ -152,6 +155,10 @@ class API extends BaseAPI {
       return cmdOutput;
     } catch (err) {
       return err.message;
+    } finally {
+      // Clear the globals after the code has run such that the next execution
+      // will be called with a clean state.
+      globals.destroy();
     }
   }
 }
