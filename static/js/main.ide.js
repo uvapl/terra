8c5c7e5ed6b7c7ecb6dc1d47f888f76b9911a64f
Kim 金可明 <koomen@protonmail.com>
2025-03-02 09:50:34 +0100

    refactor(ide): add new `app.js` with `app.ide.js`

--- static/js/main.ide.js
+++ static/js/main.ide.js
@@ -1,12 +1,7 @@
-////////////////////////////////////////////////////////////////////////////////
-// This file is the main entry point for the IDE app.
-////////////////////////////////////////////////////////////////////////////////
+Terra.app = new IDEApp();
+const layout = Terra.app.initLayout();
 
-// ===========================================================================
-// Here's the start of the application.
-// ===========================================================================
-
-initApp().then(({ layout }) => {
+layout.on('initialised', () => {
   // Fetch the repo files or the local storage files (vfs) otherwise.
   const repoLink = Terra.f.getLocalStorageItem('git-repo');
   if (repoLink) {
@@ -28,96 +23,5 @@ initApp().then(({ layout }) => {
   }
 
   $(window).resize();
-}).catch((err) => {
-  console.error('Failed to bootstrap IDE app:', err);
 });
 
-// ===========================================================================
-// Functions
-// ===========================================================================
-
-/**
- * Initialise the app by loading the config and create the layout.
- *
- * @returns {Promise<{ layout: Layout }>} Object containing the layout instance.
- */
-function initApp() {
-  return new Promise((resolve) => {
-    // Create the layout object.
-    const layout = createLayout();
-
-    // Call the init function that creates all components.
-    layout.init();
-
-    // Make layout instance available at all times.
-    Terra.layout = layout;
-
-    // Use timeout trick to make sure layout.root exists.
-    layout.on('initialised', () => {
-      resolve({ layout });
-    });
-  });
-}
-
-
-/**
- * Create the layout object with the given content objects and font-size.
- *
- * @param {boolean} [forceDefaultLayout=false] Whether to force the default layout.
- * @param {Array} [contentConfig=[]] The content configuration for the layout.
- * @returns {Layout} The layout instance.
- */
-function createLayout(forceDefaultLayout = false, contentConfig = []) {
-  const defaultContentConfig = contentConfig.map((tab) => ({
-    type: 'component',
-    componentName: 'editor',
-    componentState: {
-      fontSize: Terra.c.BASE_FONT_SIZE,
-      ...tab.componentState,
-    },
-    title: 'Untitled',
-    ...tab,
-  }))
-
-  const defaultLayoutConfig = {
-    settings: {
-      showCloseIcon: false,
-      showPopoutIcon: false,
-      showMaximiseIcon: true,
-      reorderEnabled: true,
-    },
-    dimensions: {
-      headerHeight: 30,
-      borderWidth: 10,
-    },
-    content: [
-      {
-        type: 'column',
-        content: [
-          {
-            type: 'stack',
-            content: defaultContentConfig.length > 0 ? defaultContentConfig : [
-              {
-                type: 'component',
-                componentName: 'editor',
-                componentState: {
-                  fontSize: Terra.c.BASE_FONT_SIZE,
-                },
-                title: 'Untitled',
-              },
-            ],
-          },
-          {
-            type: 'component',
-            componentName: 'terminal',
-            componentState: { fontSize: Terra.c.BASE_FONT_SIZE },
-            isClosable: false,
-            reorderEnabled: false,
-          }
-        ]
-      }
-    ]
-  };
-
-  return new LayoutIDE(defaultLayoutConfig, { forceDefaultLayout });
-}
