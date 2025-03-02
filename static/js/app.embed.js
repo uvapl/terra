010e80afdf5cf9ca5f2dd77696bc65441686c0eb
Kim 金可明 <koomen@protonmail.com>
2025-03-02 10:10:34 +0100

    refactor(embed): add new `app.embed.js`

---
+++ static/js/app.embed.js
@@ -0,0 +1,51 @@
+class EmbedApp extends App {
+  initLayout() {
+    console.log('initLayout() called from EmbedApp')
+    const queryParams = Terra.f.parseQueryParams();
+    if (typeof queryParams.filename !== 'string') {
+      throw Error('No filename provided in query params');
+    }
+
+    const isHorizontal = queryParams.layout === 'horizontal';
+    const isVertical = !isHorizontal;
+
+    // Update local storage key.
+    const currentStorageKey = Terra.f.makeLocalStorageKey(window.location.href);
+    Terra.f.updateLocalStoragePrefix(currentStorageKey);
+
+    // Create the tab in the virtual filesystem.
+    Terra.vfs.createFile({ name: queryParams.filename });
+
+    // Create tabs with the filename as key and empty string as the content.
+    const tabs = {}
+    tabs[queryParams.filename] = '';
+
+    // Get the programming language based on the filename.
+    const proglang = Terra.f.getFileExtension(queryParams.filename);
+
+    // Initialise the programming language specific worker API.
+    Terra.langWorkerApi = new LangWorkerAPI(proglang);
+
+    // Get the font-size stored in local storage or use fallback value.
+    const fontSize = Terra.f.getLocalStorageItem('font-size', Terra.c.BASE_FONT_SIZE);
+
+    // Create the content objects that represent each tab in the editor.
+    const content = this.generateConfigContent(tabs, fontSize);
+
+    // Create the layout object.
+    const layout = this.createLayout(content, fontSize, {
+      proglang,
+      vertical: isVertical,
+    });
+
+    $('body').addClass(isVertical ? 'vertical' : 'horizontal');
+
+    // Make layout instance available at all times.
+    Terra.layout = layout;
+
+    // Call the init function that creates all components.
+    layout.init();
+
+    return layout;
+  }
+}
