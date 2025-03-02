fa383f313219a05a60f35fedb3410a28d0e32d15
Kim 金可明 <koomen@protonmail.com>
2025-03-02 09:50:34 +0100

    refactor(ide): add new `app.js` with `app.ide.js`

---
+++ static/js/app.ide.js
@@ -0,0 +1,77 @@
+class IDEApp extends App {
+  initLayout() {
+    // Create the layout object.
+    const layout = this.createLayout();
+
+    // Call the init function that creates all components.
+    layout.init();
+
+    // Make layout instance available at all times.
+    Terra.layout = layout;
+
+    return layout;
+  }
+
+  /**
+   * Create the layout object with the given content objects and font-size.
+   *
+   * @param {boolean} [forceDefaultLayout=false] Whether to force the default layout.
+   * @param {Array} [contentConfig=[]] The content configuration for the layout.
+   * @returns {Layout} The layout instance.
+   */
+  createLayout(forceDefaultLayout = false, contentConfig = []) {
+    const defaultContentConfig = contentConfig.map((tab) => ({
+      type: 'component',
+      componentName: 'editor',
+      componentState: {
+        fontSize: Terra.c.BASE_FONT_SIZE,
+        ...tab.componentState,
+      },
+      title: 'Untitled',
+      ...tab,
+    }))
+
+    const defaultLayoutConfig = {
+      settings: {
+        showCloseIcon: false,
+        showPopoutIcon: false,
+        showMaximiseIcon: true,
+        reorderEnabled: true,
+      },
+      dimensions: {
+        headerHeight: 30,
+        borderWidth: 10,
+      },
+      content: [
+        {
+          type: 'column',
+          content: [
+            {
+              type: 'stack',
+              content: defaultContentConfig.length > 0 ? defaultContentConfig : [
+                {
+                  type: 'component',
+                  componentName: 'editor',
+                  componentState: {
+                    fontSize: Terra.c.BASE_FONT_SIZE,
+                  },
+                  title: 'Untitled',
+                },
+              ],
+            },
+            {
+              type: 'component',
+              componentName: 'terminal',
+              componentState: { fontSize: Terra.c.BASE_FONT_SIZE },
+              isClosable: false,
+              reorderEnabled: false,
+            }
+          ]
+        }
+      ]
+    };
+
+    return new LayoutIDE(defaultLayoutConfig, { forceDefaultLayout });
+  }
+
+}
---
+++ static/js/app.js
@@ -0,0 +1,82 @@
+/**
+ * Base class that is extended for each of the apps.
+ */
+class App {
+  constructor() {
+    this.initLayout();
+  }
+
+  initLayout() {
+    console.error('initLayout() not implemented');
+  }
+
+  /**
+   * Create a list of content objects based on the tabs config data.
+   *
+   * @param {object} tabs - An object where each key is the filename and the
+   * value is the default value the editor should have when the file is opened.
+   * @param {number} fontSize - The default font-size used for the content.
+   * @returns {array} List of content objects.
+   */
+  generateConfigContent(tabs, fontSize) {
+    return Object.keys(tabs).map((filename) => ({
+      type: 'component',
+      componentName: 'editor',
+      componentState: {
+        fontSize: fontSize,
+        value: tabs[filename],
+        fileId: Terra.f.uuidv4(),
+      },
+      title: filename,
+      isClosable: false,
+    }));
+  }
+
+  /**
+   * Create the layout object with the given content objects and font-size.
+   *
+   * @param {array} content - List of content objects.
+   * @param {number} fontSize - The default font-size to be used.
+   * @param {object} options - Additional options object.
+   * @param {boolean} options.vertical - Whether the layout should be vertical.
+   * @param {string} options.proglang - The programming language to be used
+   * @param {object} options.buttonConfig - Object containing buttons with their
+   * commands that will be rendered by the layout.
+   * @returns {Layout} The layout instance.
+   */
+  createLayout(content, fontSize, options = {}) {
+    const defaultLayoutConfig = {
+      settings: {
+        showPopoutIcon: false,
+        showMaximiseIcon: false,
+        showCloseIcon: false,
+        reorderEnabled: false,
+      },
+      dimensions: {
+        headerHeight: 30,
+        borderWidth: Terra.c.IS_FRAME ? 0 : 10,
+      },
+      content: [
+        {
+          type: options.vertical ? 'column' : 'row',
+          isClosable: false,
+          content: [
+            {
+              type: 'stack',
+              isClosable: false,
+              content: content,
+            },
+            {
+              type: 'component',
+              componentName: 'terminal',
+              componentState: { fontSize: fontSize },
+              isClosable: false,
+            }
+          ]
+        }
+      ]
+    };
+
+    return new Layout(defaultLayoutConfig, options);
+  }
+}
