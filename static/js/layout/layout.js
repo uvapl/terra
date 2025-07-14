77d1d6e213880fb13ae9de7b695398ce589cd850
Kim <koomen@protonmail.com>
2025-07-14 10:01:42 +0200

    feat: remove unnecessary console.log

--- static/js/layout/layout.js
+++ static/js/layout/layout.js
@@ -352,7 +352,6 @@ export default class Layout extends eventTargetMixin(GoldenLayout) {
    * @param {EditorComponent} editorComponent
    */
   onEditorFocus(editorComponent) {
-    console.log('FOCUS', editorComponent.getPath());
     this.setActiveEditor(editorComponent);
   }
 
