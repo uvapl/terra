651c4d9ac70cef098c224c598636db1f526e6d93
Kim <koomen@protonmail.com>
2025-10-12 16:22:47 +0200

    fix: add missing import

--- static/js/app.ide.js
+++ static/js/app.ide.js
@@ -1,12 +1,12 @@
 import App from './app.js';
 import IDELayout from './layout/layout.ide.js';
 import { MAX_FILE_SIZE } from './constants.js';
-import { getFileExtension, getRepoInfo, isBase64 } from './helpers/shared.js';
+import { getFileExtension, getRepoInfo } from './helpers/shared.js';
 import Terra from './terra.js';
 import LangWorker from './lang-worker.js';
 import { getLocalStorageItem } from './local-storage-manager.js';
 import * as fileTreeManager from './file-tree-manager.js';
-import { triggerPluginEvent } from './plugin-manager.js';
+import { triggerPluginEvent, getPlugin } from './plugin-manager.js';
 import GitFS from './fs/git.js';
 import { FileNotFoundError, FileTooLargeError } from './fs/vfs.js';
 import * as LFS from './fs/lfs.js';
